4267712ab239a86c6503989ca24b8acb1618bfb2
hoanghai0827 <honghi.hunh@gmail.com>
2022-11-20 22:08:52 +0700

    methodString_1

--- javaBasic/java.js
+++ javaBasic/java.js
@@ -1,6 +1,6 @@
-let bai5_1 = 'Hải';
-//để biến 'Hải' từ string thành đối tượng thì gõ lệnh new string("Tên")
-let bai5_2 = new String("Hải");
-//cả 2 bằng true nếu gán == VD : x == y => true === => false
 
-
+//Bài 6: string method
+let name_bai6 = 'Huynh Hoang Hai';
+let x_bai6 = name_bai6.slice(6, 11) //Lấy tên ở vị trí 6 và 11 => Hoang
+document.write(x_bai6);
+let y_bai6 = name_bai6.slice(6); document.write(y_bai6)// Lấy tên sau vị trí 6 => Hoang Hai 
