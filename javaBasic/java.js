88d36a09babbff839e48d83a1f42157ab0146cce
hoanghai0827 <honghi.hunh@gmail.com>
2022-11-20 21:55:56 +0700

    stringw3s

---
+++ javaBasic/java.js
@@ -0,0 +1,6 @@
+let bai5_1 = 'Hải';
+//để biến 'Hải' từ string thành đối tượng thì gõ lệnh new string("Tên")
+let bai5_2 = new String("Hải");
+//cả 2 bằng true nếu gán == VD : x == y => true === => false
+
+
